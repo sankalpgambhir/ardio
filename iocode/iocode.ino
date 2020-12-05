e23ab0f45e06060d3ffd52f8aecf0918232b1922
PushkarMohile <66459299+PushkarMohile@users.noreply.github.com>
2020-12-05 18:06:02 +0530

    Comments

--- iocode/iocode.ino
+++ iocode/iocode.ino
@@ -12,17 +12,17 @@ void loop() {
   // put your main code here, to run repeatedly:
 int i =0; 
 while(i< SignalSize){
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0) { //Whenever a byte is available, read it and add it to the array
     incomingString[i] = Serial.read(); 
-    
     i++;
   }
   
 }
-if(i>=100){Serial.write("N");
-digitalWrite(13,1);
-Serial.println(incomingString[200]);
- Serial.write(13);
+if(i>=SignalSize) //Check whether all expected Bytes are here
+{Serial.write("N"); 
+digitalWrite(13,1); //Glow LED for confirmation 
+Serial.println(incomingString[200]); //Send Confirmation bacl to MALAB
+ Serial.write(13); //Next two lines are terminator
   Serial.write(10);
 }
 }
