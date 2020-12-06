f871c7076cc9443ecb68e37dc2869609e3bdc09b
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2020-12-06 19:13:05 +0530

    Clean up Matlab code

--- src/getAudioAndSend2.m
+++ src/getAudioAndSend2.m
@@ -1,24 +1,35 @@
 function carr = getAudioAndSend2()
-a = audiorecorder(1000,8,1);
-recordblocking(a,2); %200 data points recorded 
-d = getaudiodata(a,'int8');
-%d = round(7.0*sin(0.6*(1:1:500)));
-indices = find(d > 7);
-d(indices) = 7;
-indices = find(d < -7);
-d(indices) = -7;
-plot(d);
+  a = audiorecorder(1000,8,1);
+  recordblocking(a,2); %2000 data points recorded 
 
-Ard = serial("COM3","BaudRate",115200);
-fopen(Ard);
-for i = 1:430
-  fprintf(Ard,'%s\n',int2str(d(i)));
-  pause(3/100);
-end
-pause(1);
-for i=1:15
-    y = fscanf(Ard,'%s');
-    fprintf('%s\n', y);
-end
-fclose(Ard);
+  d = getaudiodata(a,'int8');
+
+  % clip the data at +- 7
+  indices = find(d > 7);
+  d(indices) = 7;
+  indices = find(d < -7);
+  d(indices) = -7;
+
+  % show data
+  plot(d);
+
+  % connect to arduino
+  Ard = serial("COM3","BaudRate",115200);
+  fopen(Ard);
+
+  % send 400 and change samples
+  for i = 1:430
+    fprintf(Ard,'%s\n',int2str(d(i)));
+    pause(3/100); % prevents race condition!
+  end
+  
+  pause(1);
+
+  % read the output
+  for i=1:15
+      y = fscanf(Ard,'%s');
+      fprintf('%s\n', y);
+  end
+
+  fclose(Ard);
 end
