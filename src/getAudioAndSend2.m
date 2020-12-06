a199bdcf3a4861f0f6817512959d8a8b6eec6fee
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2020-12-06 12:42:43 +0530

    Working matlab code

--- src/getAudioAndSend2.m
+++ src/getAudioAndSend2.m
@@ -1,12 +1,24 @@
 function carr = getAudioAndSend2()
-a = audiorecorder(10000,8,1);
-recordblocking(a,1); %200 data points recorded 
-carr = getaudiodata(a,'int8')
-Ard = serial("COM4","BaudRate",9600);
+a = audiorecorder(1000,8,1);
+recordblocking(a,2); %200 data points recorded 
+d = getaudiodata(a,'int8');
+%d = round(7.0*sin(0.6*(1:1:500)));
+indices = find(d > 7);
+d(indices) = 7;
+indices = find(d < -7);
+d(indices) = -7;
+plot(d);
+
+Ard = serial("COM3","BaudRate",115200);
 fopen(Ard);
-for i = 1:200
-  fprintf(Ard,'%d\n',int2str(carr(i+1500)));
+for i = 1:430
+  fprintf(Ard,'%s\n',int2str(d(i)));
+  pause(3/100);
 end
-fscanf(Ard)
+pause(1);
+for i=1:15
+    y = fscanf(Ard,'%s');
+    fprintf('%s\n', y);
+end
 fclose(Ard);
 end
